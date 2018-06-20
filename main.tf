2439eda3ffefa725c3b14a20af7d80e00384f7d3
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2018-06-20 10:30:47 -0400

    Do things

--- main.tf
+++ main.tf
@@ -3,15 +3,9 @@ resource "random_id" "random" {
     uuid = "${uuid()}"
   }
 
-  byte_length = 10
+  byte_length = 9
 }
 
 output "random" {
   value = "${random_id.random.hex}"
-}
-
-terraform {
-  backend "atlas" {
-    name = "alisdair-v2/s3-random"
-  }
 }
