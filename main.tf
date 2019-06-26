0f1c20f52c9a401a1bc07d5cc947e38d083b317b
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2019-06-26 11:31:32 -0400

    Upgrade to 0.12

--- main.tf
+++ main.tf
@@ -7,15 +7,16 @@ variable "number" {
 }
 
 resource "random_id" "random" {
-  keepers {
-    uuid = "${uuid()}"
+  keepers = {
+    uuid = uuid()
   }
 
-  byte_length = "${var.bytes}"
-  
-  count = "${var.number}"
+  byte_length = var.bytes
+
+  count = var.number
 }
 
 output "random" {
-  value = "${random_id.random.*.hex}"
+  value = random_id.random.*.hex
 }
+
