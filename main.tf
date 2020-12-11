40787d3d7ab298ca01c6649c341f1adc564111d6
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2020-12-11 17:35:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,18 @@ variable "number" {
 resource "random_id" "random" {
   keepers = {
     uuid = uuid()
+    foo  = data.null_data_source.foo.result.bar
   }
 
   byte_length = var.bytes
 
   count = var.number
+}
+
+data "null_data_source" "foo" {
+  inputs = {
+    bar = "boop"
+  }
 }
 
 output "random" {
